97273372fde7d9c7560beb90074cc1f238e7bc76
Cauê Farias Santos <cauefsantos@MacBook-Pro-de-Caue.local>
2026-05-26 12:41:51 -0300

    chore: installs `ESLint` and creates `lint:eslint:check` script

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// eslint.config.js
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import pluginJest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier";
+
+export default defineConfig([
+  ...nextVitals,
+  prettier,
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+    },
+  },
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
