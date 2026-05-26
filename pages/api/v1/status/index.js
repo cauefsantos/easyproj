84192f3747d840dda242f0713a9c5ec456b4c802
Cauê Farias Santos <cauefsantos@MacBook-Pro-de-Caue.local>
2026-05-26 06:25:10 -0300

    feat: creates `/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,30 @@
+import database from "@/infra/database";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const oppenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const oppenedConnections = parseInt(oppenedConnectionsResult.rows[0].count);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: maxConnections,
+        oppened_connections: oppenedConnections,
+      },
+    },
+  });
+}
+
+export default status;
